19c515b8a95ed5ba64c137860fd6da58cd0664de
KozlowskiSzymon <Szymonztn7?yj>
2022-10-29 12:41:54 +0200

    init commit

---
+++ src/test/java/com/example/primenumberworker/PrimeNumberWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primenumberworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeNumberWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
