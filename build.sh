45aed9bfd098872ab30abf52bad946ce2cc5badb
Szymon Kozlowski <szymon.kozlowski@cineworldgroup.com>
2023-02-06 23:07:30 +0100

    actuator, prometheus

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-VERSION=0.0.3-SNAPSHOT
+VERSION=0.0.4-SNAPSHOT
 docker build -t szymon2kozlowski/prime-number-worker:$VERSION .
 docker push szymon2kozlowski/prime-number-worker:$VERSION
